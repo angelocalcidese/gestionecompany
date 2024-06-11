5381d1c702bcc52395aeeffe285b1be3994d03bb
acalcidese <angelo.calcidese@gmail.com>
2024-06-11 11:19:59 +0200

    pulizia + nascondi menu

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
     <div class="row">
       <?php include("../portale/menu.php"); ?>
 
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 main-page">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2">Gestione</h1>
-          
+
         </div>
 
         <!--<canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>-->
@@ -27,7 +27,7 @@
         <?php include("modal.php"); ?>
 
 
-       
+
 
         <h2>Società</h2>
         <div class="table-responsive small">
